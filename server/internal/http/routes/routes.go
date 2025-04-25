d299477814fea923bf430708f8baeb7e18cf5d47
Marcelo <marcelorcarnauba@gmail.com>
2025-04-25 11:28:36 -0300

    refac(server): grupo de rotas

--- server/internal/http/routes/routes.go
+++ server/internal/http/routes/routes.go
@@ -15,14 +15,17 @@ func NewRouteHandler(r *gin.Engine) *RouteHandler {
 }
 
 func (rh RouteHandler) UserRoutes(uh handler.UserHandler) {
-	rh.router.GET("usuarios/", uh.GetUsuarios)
-	rh.router.GET("usuarios/:id", uh.GetUsuario)
-	rh.router.DELETE("usuarios/:id", uh.DeleteUsuario)
-	rh.router.POST("usuarios/", uh.CreateUsuario)
-	rh.router.POST("usuarios/login", uh.Login)
+	usuarios := rh.router.Group("usuarios/")
+
+	usuarios.GET("/", uh.GetUsuarios)
+	usuarios.GET("/:id", uh.GetUsuario)
+	usuarios.DELETE("/:id", uh.DeleteUsuario)
+	usuarios.POST("/", uh.CreateUsuario)
+	usuarios.POST("/login", uh.Login)
 }
 
 func (rh RouteHandler) TreinoRoutes(th handler.TreinoHandler) {
-	rh.router.POST("treinos/", th.CreateTreino)
-	rh.router.GET("treinos/", th.GetTreinos)
+	treinos := rh.router.Group("treinos/")
+	treinos.POST("/", th.CreateTreino)
+	treinos.GET("/", th.GetTreinos)
 }
