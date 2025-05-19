25d24bb9f1d1dd64294b0ec12960c67035a69bf6
Marcelo <marcelorcarnauba@gmail.com>
2025-05-19 18:35:49 -0300

    feat(client): função de logout

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,14 +1,26 @@
 'use client'
 
+import { UseReload } from "@/hooks/useReload";
+import { logout } from "@/services/logout";
 import Link from "next/link";
 
 const Header: React.FC = () => {
+
+    const handleLogout = async () => {
+        try {
+            await logout()
+            UseReload()
+        } catch (err) {
+            console.log(err)
+        }
+    }
     return (
-        <header className="w-full border-b border-gray-500">
+        <header className="w-full flex justify-between border-b border-gray-500 py-4">
             <ul className="flex justify-center gap-4">
                 <li><Link href={'/'}>Home</Link></li>
                 <li><Link href={'/criar-treino'}>Treinos</Link></li>
             </ul>
+            <span className="cursor-pointer" onClick={handleLogout}>logout</span>
         </header>
     );
 };
